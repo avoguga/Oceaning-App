67bdc030cffaf842e1cabc76d7ebfccafbe7bfc2
Gustavo Henrique <avoguga@gmail.com>
2021-06-29 06:05:26 -0300

    tirando o services e deixando isso para o proprio component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Container} from './styles'
 import StartButton from '../../components/StartButton'
+
 const Home = () =>{
+  const [data,setData]=useState([]);
+
+  const getData=()=>{
+    fetch('http://localhost:3333/name'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setData(myJson);
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+
   return(
     <Container>
       <h2>
-        Time de desenvolvimento
+        Time de desenvolvimento 
       </h2>
       <h1>
         Sua evolução começa agora
