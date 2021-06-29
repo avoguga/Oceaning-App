681f02758c08da84a6b6eb09484711601789dbf5
Gustavo Henrique <avoguga@gmail.com>
2021-06-29 05:41:08 -0300

    adicionando searchSkills

--- src/services/api.js
+++ src/services/api.js
@@ -6,3 +6,12 @@ export const searchName = async (name) => {
     return data;
   } catch (err) {}
 };
+
+export const searchSkills = async (skills) => {
+  try {
+    let url = ` http://localhost:3333/skills${skills}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (err) {}
+};
